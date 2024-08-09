500ec8cdd30206d418d70511ce303b9a2f3b5446
Boris <borissoto@gmail.com>
2024-08-08 20:43:50 -0400

    first commit

---
+++ backend-spring/src/main/java/com/gamla/hoaxify/user/User.java
@@ -0,0 +1,16 @@
+package com.gamla.hoaxify.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String username;
+	
+	private String displayName;
+	
+	private String password;
+
+
+	
+}
